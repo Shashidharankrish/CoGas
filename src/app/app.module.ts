c03e81055593318d666e27488d15574e91426c6d
Shashidhar K <shashidhar.k@faststreamtech.com>
2019-03-25 11:08:27 +0530

    commits to create login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
 import { AddsecurityComponent } from './addsecurity/addsecurity.component';
 import { AddDeviceComponent } from './add-device/add-device.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminComponent,
     AddsecurityComponent,
-    AddDeviceComponent
+    AddDeviceComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
